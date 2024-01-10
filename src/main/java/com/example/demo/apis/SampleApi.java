fbcb685fc99d6c0851b8f1297222603c5e66bca9
Muneesh Kanimbakam <Kanimbakam.Muneesh@lumen.com>
2024-01-10 14:31:22 +0530

    Sample Branch Created

--- src/main/java/com/example/demo/apis/SampleApi.java
+++ src/main/java/com/example/demo/apis/SampleApi.java
@@ -10,7 +10,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class SampleApi {
     @GetMapping
     public ResponseEntity<String> get(){
-        return ResponseEntity.ok("Hello Sample");
-        return ResponseEntity.ok("Hello Master");
+        return ResponseEntity.ok("This is a Sample Branch");
     }
 }
