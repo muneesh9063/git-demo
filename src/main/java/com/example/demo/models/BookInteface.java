c37816b08c7c1ea1e95f61cdc633304ec5a95d42
Muneesh Kanimbakam <Kanimbakam.Muneesh@lumen.com>
2024-01-10 15:16:08 +0530

    Added book model & Interface

---
+++ src/main/java/com/example/demo/models/BookInteface.java
@@ -0,0 +1,10 @@
+package com.example.demo.models;
+
+public interface BookInteface {
+    void getAll();
+    void getById(int id);
+    void DeleteById(int id);
+    void UpdateById(int id);
+    void PrintSome(int start,int end);
+
+}
